ca649f8fc5ad9ae3280f64e776f28d5b6e1136df
David Mednikov <david.mednikov@gmail.com>
2020-08-16 23:03:39 -0500

    basic tab/navigation implemented using pure react js

---
+++ src/components/Community.js
@@ -0,0 +1,12 @@
+import React from 'react';
+ 
+const Community = () => {
+    return (
+       <div>
+          <h1>Community US</h1>
+          <p>Community US page body content</p>
+       </div>
+    );
+}
+ 
+export default Community;

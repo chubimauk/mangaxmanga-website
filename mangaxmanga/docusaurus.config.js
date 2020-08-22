d468ad1d0a20afd9b9485e4ddeaaf549e9370866
David Mednikov <david.mednikov@gmail.com>
2020-08-21 22:08:28 -0400

    initial docusaurus commit with main screen mostly set up

---
+++ mangaxmanga/docusaurus.config.js
@@ -0,0 +1,121 @@
+module.exports = {
+  title: 'Manga x Manga', //title in hero banner
+  tagline: 'A Manga Reader App for iOS Devices (11.0+) and MacOS (Catalina+).', //secondary title in hero banner
+  url: 'https://your-docusaurus-test-site.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  favicon: 'img/favicon.ico',
+  organizationName: 'mangaxmanga', // Usually your GitHub org/user name.
+  projectName: 'mangaxmanga', // Usually your repo name.
+  themeConfig: {
+    navbar: {
+      title: 'Manga x Manga', //title in nav bar
+      logo: {
+        alt: 'My Site Logo',
+        src: 'img/logo.svg',
+      },
+      items: [
+        {
+          to: 'docs/',
+          activeBasePath: 'docs',
+          label: 'Docs',
+          position: 'left',
+        },
+        {to: 'blog', label: 'Blog', position: 'left'},
+        {
+          href: 'https://github.com/facebook/docusaurus',
+          label: 'GitHub',
+          position: 'right',
+        },
+        {
+          label: 'Links',
+          position: 'left', // or 'right'
+          items: [
+            {
+              label: 'Reddit',
+              href: 'https://www.reddit.com/r/mangaxmanga/',
+            },
+            {
+              label: 'Discord',
+              href: 'https://discord.com/channels/725782618968293447/725782619433861152',
+            },
+            // ... more items
+          ],
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Style Guide',
+              to: 'docs/',
+            },
+            {
+              label: 'Second Doc',
+              to: 'docs/doc2/',
+            },
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'Stack Overflow',
+              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+            },
+            {
+              label: 'Discord',
+              href: 'https://discordapp.com/invite/docusaurus',
+            },
+            {
+              label: 'Twitter',
+              href: 'https://twitter.com/docusaurus',
+            },
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'Blog',
+              to: 'blog',
+            },
+            {
+              label: 'GitHub',
+              href: 'https://github.com/facebook/docusaurus',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Chubimaukinations LLC. Built with Docusaurus.`,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          // It is recommended to set document id as docs home page (`docs/` path).
+          homePageId: 'doc1',
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/facebook/docusaurus/edit/master/website/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+};
---
+++ mangaxmanga/src/pages/index.js
@@ -0,0 +1,213 @@
+import React from 'react';
+import clsx from 'clsx';
+import Layout from '@theme/Layout';
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import useBaseUrl from '@docusaurus/useBaseUrl';
+import styles from './styles.module.css';
+
+import useThemeContext from '@theme/hooks/useThemeContext';
+
+const mangaxmangafeatures = [
+  {
+    title: <>Manga x Manga</>,
+    imageUrl: 'img/logo.svg',
+    imageDarkUrl: 'img/logo.svg',
+    description: (
+      <>
+        A Manga Reader App for iOS Devices (11.0+) and MacOS (Catalina+).
+      </>
+    ),
+  }
+]
+const features = [
+  {
+    title: <>Library Tracking</>,
+    imageUrl: 'img/tracking_01_light_rp.png'/*'img/undraw_docusaurus_mountain.svg'*/,
+    imageDarkUrl: 'img/tracking_01_dark_rp.png',
+    description: (
+      <>
+        Automatically track your reading progress in your library,
+         search, and sort within it. View your library in list or a resizable grid view.
+      </>
+    ),
+  },
+  {
+    title: <>Sources</>,
+    imageUrl: 'img/sources_01_light_rp.png'/*'img/undraw_docusaurus_tree.svg'*/,
+    imageDarkUrl: 'img/sources_01_dark_rp.png',
+    description: (
+      <>
+        Online and offline reading/browsing from an ever expanding list of sources,
+        including <code>MangaDex</code>, <code>Manganelo</code>, and <code>MangaPark</code>.
+      </>
+    ),
+  },
+  {
+    title: <>Customization</>,
+    imageUrl: 'img/reading_01_light_rp.png'/*'img/undraw_docusaurus_react.svg'*/,
+    imageDarkUrl: 'img/reading_01_dark_rp.png',
+    description: (
+      <>
+        Customizable UI themes/modes in settings and customizable 
+        reading preferences and modes including paged and longstrip.
+      </>
+    ),
+  },
+];
+
+const features2 = [
+  {
+    title: <>Reading Recommendations</>,
+    imageUrl: 'img/recommendations_01_light_rp.png'/*'img/undraw_docusaurus_mountain.svg'*/,
+    imageDarkUrl: 'img/recommendations_01_dark_rp.png',
+    description: (
+      <>
+        Manga reading recommendations, updated periodically with new goodies. 
+        Themed reading lists, newly updated lists, works of accomplished authors.
+      </>
+    ),
+  },
+  {
+    title: <>Import</>,
+    imageUrl: 'img/import_01_light_rp.png'/*'img/undraw_docusaurus_tree.svg'*/,
+    imageDarkUrl: 'img/import_01_dark_rp.png',
+    description: (
+      <>
+        Import your own manga files in .zip format! Smart import will automatically figure out what you're adding!
+         Rename the manga if it doesn't correctly figure it out.
+      </>
+    ),
+  },
+  {
+    title: <>Quality of Life</>,
+    imageUrl: 'img/qol_01_light_rp.png'/*'img/undraw_docusaurus_tree.svg'*/,
+    imageDarkUrl: 'img/qol_01_dark_rp.png',
+    description: (
+      <>
+        Various QOL features - On chapter list pages you can view chapters in list/grid to save scrolling time,
+         filter by languages, sort, and jump scroll to a specific chapter. 
+      </>
+    ),
+  }
+];
+
+function Feature({imageUrl, imageDarkUrl, title, description}) {
+  const {isDarkTheme, setLightTheme, setDarkTheme} = useThemeContext();
+  const imgUrl = useBaseUrl(`${isDarkTheme ? imageDarkUrl : imageUrl}`);
+  return (
+    <div className={clsx('col col--4', styles.feature)}>
+      <h3>{title}</h3>
+      <p>{description}</p>
+      {imgUrl && (
+        <div className="text--center">
+          <img className={styles.featureImage} src={imgUrl} alt={title} />
+        </div>
+      )}
+      
+    </div>
+  );
+}
+
+function MangaFeature({imageUrl, title, description}) {
+  const imgUrl = useBaseUrl(imageUrl);
+  return (
+    <div className={clsx('col col--4', styles.feature)}>
+      {imgUrl && (
+        <div className="text--center">
+          <img className={styles.featureImage} src={imgUrl} alt={title} />
+        </div>
+      )}
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </div>
+  );
+}
+
+function Home() {
+  const context = useDocusaurusContext();
+  const {siteConfig = {}} = context;
+  return (
+    <Layout
+      title={`Hello from ${siteConfig.title}`}
+      description="Description will go into a meta tag in <head />">
+      <header className={clsx('', styles.heroBanner)}> {/*hero hero--primary --use this for class to use ifm colors from custom.css*/}
+        <div className="container">
+          <img className={styles.headerImage} src="/img/logo.svg" alt="AltTitle" />
+          <h1 className="hero__title">{siteConfig.title}</h1>
+          <p className="hero__subtitle">{siteConfig.tagline}</p>
+          <div className={styles.buttons}>
+            <Link
+              className={clsx(
+                'button button--outline button--secondary button--lg',
+                styles.getStarted,
+              )}
+              to={useBaseUrl('docs/')}>
+              Get Started
+            </Link>
+          </div>
+        </div>
+      </header>
+      <main>
+        {features && features.length > 0 && (
+          <section className={styles.features}>
+            <div className="container">
+              <div className="row">
+                {features.map((props, idx) => (
+                  <Feature key={idx} {...props} />
+                ))}
+              </div>
+            </div>
+          </section>
+        )}
+        {features2 && features2.length > 0 && (
+          <section className={styles.features}>
+            <div className="container">
+              <div className="row">
+                {features2.map((props, idx) => (
+                  <Feature key={idx} {...props} />
+                ))}
+              </div>
+            </div>
+          </section>
+        )}
+        {mangaxmangafeatures && mangaxmangafeatures.length > 0 && (
+          <section className={styles.features}>
+            <div className="container">
+              <div className="row">
+                {mangaxmangafeatures.map((props, idx) => (
+                  <Feature key={idx} {...props} />
+                ))}
+              </div>
+            </div>
+          </section>
+        )}
+        <section className={styles.features}>
+          <div className="container">
+            <div className="row">
+            <Feature
+              title={<>Manga x Manga</>}
+              imageUrl="img/logo.svg"
+              imageDarkUrl="img/logo.svg"
+              description={<>A Manga Reader App for iOS and MacOS.</>}
+            />
+            </div>
+          </div>
+        </section>
+        {mangaxmangafeatures && mangaxmangafeatures.length > 0 && (
+          <section className={styles.features}>
+            <div className="container">
+              <div className="column">
+                {mangaxmangafeatures.map((props, idx) => (
+                  <Feature key={idx} {...props} />
+                ))}
+              </div>
+            </div>
+          </section>
+        )}
+      </main>
+    </Layout>
+  );
+}
+
+export default Home;
